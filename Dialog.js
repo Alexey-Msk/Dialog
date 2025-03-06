34a0669e8f8edc97a2ce35f79a0ede83ab8b4819
Alexey B <alexey.v.b.mail@gmail.com>
2025-03-07 00:41:02 +0300

    Поддержка множественных диалогов.

--- Dialog.js
+++ Dialog.js
@@ -5,9 +5,10 @@
 class Dialog {
 // export default class Dialog {
 
-	static #isActive = false;
-	static #isModal = false;
-	static #callback = null;
+	#isActive = false;
+	#isModal = false;
+	#callback = null;
+	#element = null;
 
 	/** Используемые имена классов. */
 	static classNames = {
@@ -23,8 +24,13 @@ class Dialog {
 	static minContentHeight = 30;
 
 	/** Возвращает логическое значение, указывающее, открыт ли диалог в данный момент. */
-	static get isActive() {
+	get isActive() {
 		return this.#isActive;
+	}
+
+	/** Возвращает DOM-элемент диалога. */
+	get element() {
+		return this.#element;
 	}
 
 	/**
@@ -44,8 +50,8 @@ class Dialog {
 	 * @param {number} options.maxHeight Максимальная высота окна.
 	 * @param {number} options.minContentHeight Минимальная высота содержимого.
 	 */
-	static show(id, modal, content, options = {}) {
-		const { header, buttons, callback, width, height, maxWidth, maxHeight, minContentHeight = this.minContentHeight } = options;
+	show(id, modal, content, options = {}) {
+		const { header, buttons, callback, width, height, maxWidth, maxHeight, minContentHeight = Dialog.minContentHeight } = options;
 
 		if (typeof modal != "boolean")
 			throw new TypeError("Параметр modal должен быть типа boolean.");
@@ -75,11 +81,11 @@ class Dialog {
 		let headerCode = "", buttonsCode = "", styleCode = "";
 
 		if (header) {
-			headerCode = `<div class="${this.classNames.header}">${header}<div class="${this.classNames.closeButton}" title="Закрыть" data-value="#close"><svg width="16" height="16" viewBox="0 0 14 14" role="img" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="m 12,10.047142 q 0,0.3367 -0.235692,0.572383 l -1.144783,1.144783 Q 10.383842,12 10.047142,12 9.7104417,12 9.47475,11.764308 L 7,9.289558 4.52525,11.764308 Q 4.2895583,12 3.9528583,12 3.6161583,12 3.380475,11.764308 L 2.2356917,10.619525 Q 2,10.383842 2,10.047142 2,9.710442 2.2356917,9.47475 L 4.7104417,7 2.2356917,4.52525 Q 2,4.2895583 2,3.9528583 2,3.6161583 2.2356917,3.380475 L 3.380475,2.2356917 Q 3.6161583,2 3.9528583,2 4.2895583,2 4.52525,2.2356917 L 7,4.7104417 9.47475,2.2356917 Q 9.7104417,2 10.047142,2 q 0.3367,0 0.572383,0.2356917 L 11.764308,3.380475 Q 12,3.6161583 12,3.9528583 12,4.2895583 11.764308,4.52525 L 9.2895583,7 11.764308,9.47475 Q 12,9.710442 12,10.047142 z"/></svg></div></div>`;
+			headerCode = `<div class="${Dialog.classNames.header}">${header}<div class="${Dialog.classNames.closeButton}" title="Закрыть" data-value="#close"><svg width="16" height="16" viewBox="0 0 14 14" role="img" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="m 12,10.047142 q 0,0.3367 -0.235692,0.572383 l -1.144783,1.144783 Q 10.383842,12 10.047142,12 9.7104417,12 9.47475,11.764308 L 7,9.289558 4.52525,11.764308 Q 4.2895583,12 3.9528583,12 3.6161583,12 3.380475,11.764308 L 2.2356917,10.619525 Q 2,10.383842 2,10.047142 2,9.710442 2.2356917,9.47475 L 4.7104417,7 2.2356917,4.52525 Q 2,4.2895583 2,3.9528583 2,3.6161583 2.2356917,3.380475 L 3.380475,2.2356917 Q 3.6161583,2 3.9528583,2 4.2895583,2 4.52525,2.2356917 L 7,4.7104417 9.47475,2.2356917 Q 9.7104417,2 10.047142,2 q 0.3367,0 0.572383,0.2356917 L 11.764308,3.380475 Q 12,3.6161583 12,3.9528583 12,4.2895583 11.764308,4.52525 L 9.2895583,7 11.764308,9.47475 Q 12,9.710442 12,10.047142 z"/></svg></div></div>`;
 		}
 
 		if (buttons) {
-			buttonsCode = `<div class="${this.classNames.buttonsContainer}">`;
+			buttonsCode = `<div class="${Dialog.classNames.buttonsContainer}">`;
 			for (let key in buttons)
 				buttonsCode += `<button data-value="${key}">${buttons[key]}</button>`;
 			buttonsCode += '</div>';
@@ -103,21 +109,22 @@ class Dialog {
 		}
 
 		let idAttr = id ? `id="${id}"` : "";
-		let html = `<div ${idAttr} class="${this.classNames.dialog}"${styleCode}>
+		let html = `<div ${idAttr} class="${Dialog.classNames.dialog}"${styleCode}>
 						<div>
 							${headerCode}
-							<div class="${this.classNames.content}">${content}</div>
+							<div class="${Dialog.classNames.content}">${content}</div>
 							${buttonsCode}
 						</div>
 					</div>`;
 		if (modal)
-			html = `<div class="${this.classNames.modalBlocker}">${html}</div>`;
+			html = `<div class="${Dialog.classNames.modalBlocker}">${html}</div>`;
 
 		document.body.insertAdjacentHTML("beforeend", html);
 
 		let element = document.body.lastElementChild;
 		if (modal)
 			element = element.firstElementChild;
+		this.#element = element;
 		const container = element.firstElementChild;
 		// container.style.maxHeight = element.clientHeight + "px";
 
@@ -128,8 +135,8 @@ class Dialog {
 
 		const autoSize = !width && !height;
 		if (autoSize) {
-			// const headerHeight = element.querySelector("." + this.classNames.header)?.offsetHeight;
-			// const controlsHeight = element.querySelector("." + this.classNames.buttonsContainer)?.offsetHeight;
+			// const headerHeight = element.querySelector("." + Dialog.classNames.header)?.offsetHeight;
+			// const controlsHeight = element.querySelector("." + Dialog.classNames.buttonsContainer)?.offsetHeight;
 			// const minHeight = minContentHeight + (headerHeight ?? 0) + (controlsHeight ?? 0);
 			// element.style.minHeight = minHeight + "px";
 			updateSizeVariables();
@@ -143,30 +150,36 @@ class Dialog {
 			}
 		});
 
-		setTimeout(() => document.addEventListener("click", this.#documentClickHandler), 0);
+		setTimeout(() => {
+			const buttonsContainer = container.getElementsByClassName(Dialog.classNames.buttonsContainer).item(0);
+			const closeButton = container.getElementsByClassName(Dialog.classNames.closeButton).item(0);
+			buttonsContainer?.addEventListener("click", this.#clickHandler);
+			closeButton?.addEventListener("click", this.#clickHandler);
+		}, 0);
+
 		this.#isActive = true;
 	}
 
 	/** Закрывает диалог. */
-	static hide() {
+	hide() {
 		if (!this.#isActive) return;
-		document.querySelector("." + (this.#isModal ? this.classNames.modalBlocker : this.classNames.dialog)).remove();
-		document.removeEventListener("click", this.#documentClickHandler);
+		(this.#isModal ? this.#element.parentElement : this.#element).remove();
+		// document.removeEventListener("click", this.#clickHandler);
 		this.#isActive = false;
 	}
 
-	static #documentClickHandler = (event) => {
+	#clickHandler = (event) => {
 		let target = event.target;
-		if (target.closest("." + this.classNames.dialog)) {
-			if (target.tagName == "BUTTON" || target.matches("." + this.classNames.closeButton)) {
+		if (target.closest("." + Dialog.classNames.dialog)) {
+			if (target.tagName == "BUTTON" || target.matches("." + Dialog.classNames.closeButton)) {
 				let hide = true;
-				if (Dialog.#callback)
-					hide = Dialog.#callback(target.dataset.value);
+				if (this.#callback)
+					hide = this.#callback(target.dataset.value);
 				if (hide !== false)
-					Dialog.hide();
+					this.hide();
 			}
 		}
-		else if (!Dialog.#isModal)
-			Dialog.hide();
+		else if (!this.#isModal)
+			this.hide();
 	};
 }
